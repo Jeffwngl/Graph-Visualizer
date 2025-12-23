1f09a21a08f984e5019b7bb60a03fd5c95d8af38
Jeff Ubuntu <{ID}+{username}@users.noreply.github.com>
2025-12-24 09:53:40 +1100

    Initial commit

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -6,12 +6,27 @@ import { useState } from 'react'
 export default function RightMenu() {
     const [isOpen, setIsOpen] = useState(false)
 
+    const dfsDisplay = () => {
+        return (
+            <div className='codeDisplay'>
+                <p>dfs(v):</p>
+                <p>if (visited has current v):</p>
+                <p>   return</p>
+                <p>visited.add(v)</p>
+                <p>for (neighbours in v):</p>
+                <p>   if (neighbour not visited):</p>
+                <p>      dfs(neighbour)</p>
+            </div>
+        )
+    }
+
     return (
         <div className={`RightMenu ${isOpen ? "open" : "closed"}`}>
             <button className='ToggleRight' id='toggleRight' onClick={() => setIsOpen(!isOpen)}>{isOpen ? <RightArrowIcon /> : <LeftArrowIcon /> }</button>
             {isOpen && (
                 <div className="StepsMenu">
                     <p>Steps:</p>
+                    {dfsDisplay()}
                 </div>
             )}
         </div>
